6812387a6d40a1bdaeaf6b0b67d7d8409a034a82
baba_s <shotababa1206@gmail.com>
2020-04-19 15:23:06 +0900

    add comment

--- Scripts/ISequenceTask.cs
+++ Scripts/ISequenceTask.cs
@@ -3,30 +3,54 @@ using System.Collections;
 
 namespace UniSequenceTask
 {
+	/// <summary>
+	/// タスクのインターフェイス
+	/// </summary>
 	public interface ISequenceTask : IEnumerable
 	{
+		/// <summary>
+		/// タスクを追加します
+		/// </summary>
 		void Add( string text, Action<Action> task );
-
+		
+		/// <summary>
+		/// タスクを実行します
+		/// </summary>
 		void Play( string text, Action onCompleted );
 	}
 
+	/// <summary>
+	/// ISequenceTask 型の拡張メソッドを管理するクラス
+	/// </summary>
 	public static class ISequenceTaskExt
 	{
+		/// <summary>
+		/// タスクを追加します
+		/// </summary>
 		public static void Add( this ISequenceTask self, Action<Action> task )
 		{
 			self.Add( string.Empty, task );
 		}
-
+		
+		/// <summary>
+		/// タスクを実行します
+		/// </summary>
 		public static void Play( this ISequenceTask self )
 		{
 			self.Play( onCompleted: null );
 		}
-
+		
+		/// <summary>
+		/// タスクを実行します
+		/// </summary>
 		public static void Play( this ISequenceTask self, Action onCompleted )
 		{
 			self.Play( string.Empty, onCompleted );
 		}
-
+		
+		/// <summary>
+		/// タスクを実行します
+		/// </summary>
 		public static void Play( this ISequenceTask self, string text )
 		{
 			self.Play( text, null );
